bf22ab5af303101ae8b74925bb2f64594a5f11f1
Bror Lundberg <maximillian.lundber@chasacademy.se>
2025-06-27 10:43:41 +0200

    lalal

--- dynamisk-cd-final/src/main.jsx
+++ dynamisk-cd-final/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import './style.css';
 
 import HomePage from './HomePage';
@@ -10,17 +10,15 @@ import ProjectsPage from './ProjectsPage';
 import ContactPage from './ContactPage';
 import NotFoundPage from './NotFoundPage';
 
-const basename = import.meta.env.DEV ? '/' : '/cv-react/';
-
 createRoot(document.getElementById('root')).render(
-<BrowserRouter basename={import.meta.env.BASE_URL}>
-  <Routes>
-    <Route path="" element={<HomePage />} />
-    <Route path="about" element={<AboutPage />} />
-    <Route path="cv" element={<CVPage />} />
-    <Route path="project" element={<ProjectsPage />} />
-    <Route path="contact" element={<ContactPage />} />
-    <Route path="*" element={<NotFoundPage />} />
-  </Routes>
-</BrowserRouter>
+  <HashRouter>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/about" element={<AboutPage />} />
+      <Route path="/cv" element={<CVPage />} />
+      <Route path="/project" element={<ProjectsPage />} />
+      <Route path="/contact" element={<ContactPage />} />
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
+  </HashRouter>
 );
