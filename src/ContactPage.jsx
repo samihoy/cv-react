404cbb3199c79053fe8f377f7036db4db55fbcc5
samihoy <maximillian.lundberg@chasacademy.se>
2026-02-18 23:01:13 +0100

    removed unecesary elements in contact page

--- src/ContactPage.jsx
+++ src/ContactPage.jsx
@@ -4,18 +4,22 @@ export default function ContactPage() {
     return (
     <>
       <header className="top">
-        <div className="nav">
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About</Link></li>
-          <li><Link to="/cv">CV</Link></li>
-          <li><Link to="/project">Project</Link></li>
-          <li><Link className="active" to="/contact">Contact</Link></li>
-        </div>
+        <nav>
+          <ul className="nav">
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/cv">CV</Link></li>
+            <li><Link className='active' to="/project">Project</Link></li>
+            <li><Link to="/contact">Contact</Link></li>
+            <li><Link to="/blogg">Blogg</Link></li>
+            <li><Link to="/easteregg">easteregg</Link></li>
+          </ul>
+        </nav>
       </header>
 
       <footer className="footer">
         <div className="contact">
-          <h3 className="title">contact</h3>
+  
           <div className="contact_info">
             <p className="description easteregg">Porthansvägen 12</p>
             <p className="description">0707254421</p>
